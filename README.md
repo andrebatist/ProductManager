9b0782214d56b10d689c72ed389862afe72de0a5
aplaksin <aplaksin@ecocompany.ru>
2020-01-10 17:32:01 +0300

    CRUD finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # crud_mvc
+Product entity all CRUD operations with Spring Boot, MySQL, Hibernate, Thymeleaf
+https://www.youtube.com/watch?v=QloyS2dt9T4
--- src/main/java/net/code/java/ProductManager/controller/ProductController.java
+++ src/main/java/net/code/java/ProductManager/controller/ProductController.java
@@ -5,7 +5,11 @@ import net.code.java.ProductManager.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -21,5 +25,30 @@ public class ProductController {
         return "index";
     }
 
+    @RequestMapping("/new")
+    public String showNewProductForm(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "new_product";
+    }
 
+    @PostMapping("/save")
+    public String saveProduct(@ModelAttribute("product") Product product) {
+        service.save(product);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductForm(@PathVariable(name = "id") Long id) {
+        ModelAndView mav = new ModelAndView("edit_product");
+        Product product = service.get(id);
+        mav.addObject("product",product);
+        return mav;
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable(name = "id") Long id) {
+        service.delete(id);
+        return "redirect:/";
+    }
 }
